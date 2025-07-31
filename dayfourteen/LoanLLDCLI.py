816416240f8c10344f7a34fd7295ec819a1db3eb
razzaksr <razzaksr@gmail.com>
2025-07-31 11:48:42 +0530

    multilingual tamil, hindi, kannada and english implemented

--- dayfourteen/LoanLLDCLI.py
+++ dayfourteen/LoanLLDCLI.py
@@ -1,10 +1,19 @@
+import argparse
 from model import Loan
 import os
 from dotenv import load_dotenv
 from LoanService import *
+from pathlib import Path
 
-load_dotenv()
-
+parser = argparse.ArgumentParser(description='Loan Multilingual App')
+parser.add_argument('--lang', 
+                    choices=['en', 'hi', 'ta', 'kn'], 
+                    default='en',
+                    help='Language code')
+args = parser.parse_args()
+# Load the appropriate language file
+env_file = f"{args.lang}.env"
+load_dotenv(env_file,override=True)
 print(os.getenv("WELCOME"))
 data_source = input(os.getenv("SELECT_SOURCE"))
 service=LoanService(data_source)
